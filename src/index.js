de383be3a9789cdca2feb01b2c84a1ab302e0a51
DanyalS <danyalsoharwardi@icloud.com>
2024-04-02 00:42:12 -0400

    Fixed routing issue

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import { createRoot } from "react-dom";
 import App from "./App";
 import SongLyrics from "./components/SongLyrics";
 
 ReactDOM.render(
   <Router>
     <Switch>
-      <Route exact path="/" component={App} />
+      createRoot(document.getElementById("root")).render(
+      <App />
+      );
       <Route path="/song-lyrics" component={SongLyrics} />
     </Switch>
   </Router>,
