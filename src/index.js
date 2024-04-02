0216be7a3e4afd31fb4882b724b9188708c86083
DanyalS <danyalsoharwardi@icloud.com>
2024-04-02 01:04:13 -0400

    Fixed render issue

--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
-import { createRoot } from "react-dom";
 import App from "./App";
 import SongLyrics from "./components/SongLyrics";
 
 ReactDOM.render(
   <Router>
     <Switch>
-      createRoot(document.getElementById("root")).render(
+      createRoot(document.getElementById('root')).render(
       <App />
       );
       <Route path="/song-lyrics" component={SongLyrics} />
